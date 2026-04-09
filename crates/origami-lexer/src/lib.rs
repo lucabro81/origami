e8bc763008c4fc97de12cce31c2937217b7bcf4e
Luca Brognara <luca.brognara81@gmail.com>
2026-04-09 23:32:28 +0200

    feat: first version of lexer

--- crates/origami-lexer/src/lib.rs
+++ crates/origami-lexer/src/lib.rs
@@ -1,7 +1,30 @@
-use origami_runtime::{LexError, Token};
+use logos::Logos;
+use origami_runtime::{LexError, Position, Token, codes};
+
+fn offset_to_pos(input: &str, offset: usize) -> Position {
+  let before = &input[..offset];
+  let line = before.chars().filter(|&c| c == '\n').count() + 1;
+  let col = before.rfind('\n').map(|i| offset - i - 1).unwrap_or(offset) + 1;
+  Position { line, col}
+}
 
 pub fn lex(input: &str) -> Result<Vec<Token>, LexError> {
-  Ok(vec![Token::Eof])
+  let mut lexer = Token::lexer(input);
+  let mut tokens = vec![];
+  while let Some(result) = lexer.next() {
+    match result {
+      Ok(token) => tokens.push(token),
+      Err(_) => {
+        let pos = offset_to_pos(input, lexer.span().start);
+        return Err(LexError::UnexpectedChar { 
+        code: codes::L002.code , 
+        message: codes::L002.message, 
+        pos
+      })}
+    }
+  }
+  tokens.push(Token::Eof);
+  Ok(tokens)
 }
 
 #[cfg(test)] mod tests;

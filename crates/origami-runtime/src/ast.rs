42f2acb93a27212386ef04f1837fcd06cfbab982
Luca Brognara <luca.brognara81@gmail.com>
2026-04-15 20:15:55 +0200

    feat: new ast file

---
+++ crates/origami-runtime/src/ast.rs
@@ -0,0 +1,5 @@
+#[derive(Debug, PartialEq)]
+pub struct Prop {
+    pub name: String,
+    pub type_str: String
+}
--- crates/origami-runtime/src/lib.rs
+++ crates/origami-runtime/src/lib.rs
@@ -1,8 +1,12 @@
 pub mod tokens;
+pub mod ast;
 pub mod errors;
 pub mod position;
 pub mod codes;
 
 pub use tokens::Token;
+pub use ast::Prop;
+
 pub use errors::LexError;
 pub use position::Position;
+
